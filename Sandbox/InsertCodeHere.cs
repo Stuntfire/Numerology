0747c582a420c6f573fff097d8018e574cea79e0
Søren Roed Schack <soer622t@edu.easj.dk>
2016-10-11 13:56:50 +0200

    Tilføjede et nyt objekt: myMysticNumbers og testede metoden ThreeNumbers.

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,7 +7,11 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
-
+            MysticNumbers myMysticNumbers = new MysticNumbers();
+            // myMysticNumbers.ThreeNumbers(3,2,1);
+            Console.WriteLine(myMysticNumbers.ThreeNumbers(3, 2, 1));
+            Console.WriteLine(myMysticNumbers.ThreeNumbers(1, 3, 2));
+            Console.WriteLine(myMysticNumbers.ThreeNumbers(1, 1, 2));
             // The LAST line of code should be ABOVE this line
         }
     }
